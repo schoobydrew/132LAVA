5bc41569a7e3d47c93036d0c7cda643f57b0236d
schoobydrew <36652374+schoobydrew@users.noreply.github.com>
2018-05-02 13:52:53 -0500

    Add files via upload

---
+++ Photoresistor.ino
@@ -0,0 +1,12 @@
+int val = 0;
+int photoresistor_pin = 5; //might change pin for photoresistor wiring
+void setup() {
+  //set serial monitor bit rate to 9600
+  Serial.begin(9600);
+}
+
+void loop() {
+  //save photoresitor valuse to photoresistor output
+  val = analogRead(photoresistor_pin);
+  Serial.println(val); //print values on new line
+}
